3976860d4a821c080a20c8b7666440eabdc4b78c
scott fuston <fuston05@yahoo.com>
2021-01-11 19:14:10 -0500

    install older node-sass version for compatibility testing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './sass/index.scss';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
